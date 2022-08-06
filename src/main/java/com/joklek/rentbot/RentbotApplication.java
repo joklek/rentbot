2d013d00e8e31d9bed4f01c2db20d77065b3b8fe
joklek <jokubas.lekevicius@gmail.com>
2022-08-06 16:59:21 +0300

    Move web driver setup outside main

--- src/main/java/com/joklek/rentbot/RentbotApplication.java
+++ src/main/java/com/joklek/rentbot/RentbotApplication.java
@@ -1,6 +1,5 @@
 package com.joklek.rentbot;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RentbotApplication {
 
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-//        var posts = new AruodasScraper().getLatestPosts();
-//        System.out.println(posts);
         SpringApplication.run(RentbotApplication.class, args);
     }
 }
--- src/main/java/com/joklek/rentbot/config/ScheduleConfig.java
+++ src/main/java/com/joklek/rentbot/config/ScheduleConfig.java
@@ -1,9 +1,17 @@
 package com.joklek.rentbot.config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableScheduling
 public class ScheduleConfig {
+
+    @PostConstruct
+    public void initScraper() {
+        WebDriverManager.chromedriver().setup();
+    }
 }
