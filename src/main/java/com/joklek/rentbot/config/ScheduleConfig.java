1ea7150630f602d85c360d3209ef27dbe6879e28
joklek <jokubas.lekevicius@gmail.com>
2022-08-07 15:07:46 +0300

    Selenium, silence!

--- src/main/java/com/joklek/rentbot/config/ScheduleConfig.java
+++ src/main/java/com/joklek/rentbot/config/ScheduleConfig.java
@@ -1,6 +1,7 @@
 package com.joklek.rentbot.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,5 +16,6 @@ public class ScheduleConfig {
     @PostConstruct
     public void initScraper() {
         WebDriverManager.chromedriver().setup();
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
     }
 }
