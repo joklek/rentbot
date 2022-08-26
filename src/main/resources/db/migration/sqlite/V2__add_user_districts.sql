f27f735d38315b43cdd8865df1cd2c974329cfa2
joklek <jokubas.lekevicius@gmail.com>
2022-08-27 00:05:03 +0300

    Backport SQL fix

--- src/main/resources/db/migration/sqlite/V2__add_user_districts.sql
+++ src/main/resources/db/migration/sqlite/V2__add_user_districts.sql
@@ -2,6 +2,6 @@ CREATE TABLE user_districts
 (
     user_id     INTEGER NOT NULL,
     district_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (telegram_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (district_id) REFERENCES districts (id)
 );
