ecec1e696a328a5e313bbf0841909a4fa26e05d7
joklek <jokubas.lekevicius@gmail.com>
2024-09-21 20:51:36 +0300

    Implement easily configurable district pages

--- src/main/java/com/joklek/rentbot/bot/providers/DistrictsPageProvider.java
+++ src/main/java/com/joklek/rentbot/bot/providers/DistrictsPageProvider.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 public class DistrictsPageProvider {
     private final DistrictRepo districts;
 
+    public static final int ROW_COUNT = 3;
+    public static final int ROW_LENGTH = 3;
+    public static final int PAGE_SIZE = ROW_COUNT * ROW_LENGTH;
+
     public DistrictsPageProvider(DistrictRepo districts) {
         this.districts = districts;
     }
@@ -27,36 +31,22 @@ public class DistrictsPageProvider {
         var keyboard = new InlineKeyboardMarkup();
         var allDistricts = districts.findAllByOrderByNameAsc();
         var userDistricts = districts.findByUsers(user);
-        var pageSize = 9;
-        var rowSize = 3;
-        var pageCount = allDistricts.size() / pageSize;
+        var pageCount = allDistricts.size() / PAGE_SIZE;
         var nextPage = Math.min(page + 1, pageCount);
         var prevPage = Math.max(page - 1, 0);
 
-        var fromIndex = page * pageSize;
+        var fromIndex = page * PAGE_SIZE;
 
-        var firstRowDistricts = allDistricts.stream().skip(fromIndex).limit(rowSize).toList();
-        var secondRowDistricts = allDistricts.stream().skip(fromIndex + rowSize).limit(rowSize).toList();
-        var thirdRowDistricts = allDistricts.stream().skip(fromIndex + rowSize + rowSize).limit(rowSize).toList();
-
-        var firstRowButtons = firstRowDistricts.stream().map(district -> {
-            var name = userDistricts.contains(district) ? String.format("✅%s", district.getName()) : district.getName();
-            var districtButton = new InlineKeyboardButton(name);
-            districtButton.callbackData(DistrictsCallback.Toggle.callbackKey(district.getId()));
-            return districtButton;
-        }).toList();
-        var secondRowButtons = secondRowDistricts.stream().map(district -> {
-            var name = userDistricts.contains(district) ? String.format("✅%s", district.getName()) : district.getName();
-            var districtButton = new InlineKeyboardButton(name);
-            districtButton.callbackData(DistrictsCallback.Toggle.callbackKey(district.getId()));
-            return districtButton;
-        }).toList();
-        var thirdRowButtons = thirdRowDistricts.stream().map(district -> {
-            var name = userDistricts.contains(district) ? String.format("✅%s", district.getName()) : district.getName();
-            var districtButton = new InlineKeyboardButton(name);
-            districtButton.callbackData(DistrictsCallback.Toggle.callbackKey(district.getId()));
-            return districtButton;
-        }).toList();
+        for (var i = 0L; i < ROW_COUNT; i++) {
+            var rowDistricts = allDistricts.stream().skip(fromIndex + ROW_LENGTH * i).limit(ROW_LENGTH).toList();
+            var rowButtons = rowDistricts.stream().map(district -> {
+                var name = userDistricts.contains(district) ? String.format("✅%s", district.getName()) : district.getName();
+                var districtButton = new InlineKeyboardButton(name);
+                districtButton.callbackData(DistrictsCallback.Toggle.callbackKey(district.getId()));
+                return districtButton;
+            }).toList();
+            keyboard.addRow(rowButtons.toArray(InlineKeyboardButton[]::new));
+        }
 
         var prevButton = new InlineKeyboardButton("⬅");
         prevButton.callbackData(DistrictsCallback.Page.callbackKey(prevPage));
@@ -66,9 +56,6 @@ public class DistrictsPageProvider {
         resetButton.callbackData(DistrictsCallback.Reset.CALLBACK_KEY);
         var turnOffButton = new InlineKeyboardButton("❌");
         turnOffButton.callbackData(DistrictsCallback.TurnOff.CALLBACK_KEY);
-        keyboard.addRow(firstRowButtons.toArray(InlineKeyboardButton[]::new));
-        keyboard.addRow(secondRowButtons.toArray(InlineKeyboardButton[]::new));
-        keyboard.addRow(thirdRowButtons.toArray(InlineKeyboardButton[]::new));
         keyboard.addRow(prevButton, nextButton, resetButton, turnOffButton); // control row
         return keyboard;
     }
