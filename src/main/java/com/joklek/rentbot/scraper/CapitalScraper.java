e970c99689cae4c7790eca6af6ac3e098c3d726c
joklek <jokubas.lekevicius@gmail.com>
2024-09-23 20:46:52 +0300

    Fix capital ignoring sold flats

--- src/main/java/com/joklek/rentbot/scraper/CapitalScraper.java
+++ src/main/java/com/joklek/rentbot/scraper/CapitalScraper.java
@@ -27,7 +27,7 @@ public class CapitalScraper extends JsoupScraper {
         }
         var doc = maybeDoc.get();
 
-        var rawPosts = doc.select("div.realty-items > a:not(.realty-status-rented)");
+        var rawPosts = doc.select("div.realty-items > a:not(.realty-status-sold)");
 
         return rawPosts.stream()
                 .map(rawPost -> processItem(rawPost))
