f2d192e1e835cf332119f03eb1b9ded760258088
joklek <jokubas.lekevicius@gmail.com>
2024-09-28 15:50:26 +0300

    Create a virtual post out of duplicates for representation

--- src/main/java/com/joklek/rentbot/bot/PostResponseCreator.java
+++ src/main/java/com/joklek/rentbot/bot/PostResponseCreator.java
@@ -19,7 +19,7 @@ public class PostResponseCreator {
         var sb = new StringBuilder();
         posts.forEach(post -> sb.append(String.format("%d. %s\n", post.getId(), post.getLink())));
 
-        var post = posts.getFirst();
+        var post = getPostForRepresentation(posts);
         var address = getAddress(post);
         if (address.isPresent()) {
             sb.append(String.format("» *Address:* [%s](https://maps.google.com/?q=%s)\n", address.get(), URLEncoder.encode(address.get(), StandardCharsets.UTF_8)));
@@ -62,6 +62,52 @@ public class PostResponseCreator {
         return new SendMessage(telegramId, sb.toString())
                 .parseMode(ParseMode.Markdown)
                 .linkPreviewOptions(new LinkPreviewOptions().isDisabled(true));
+    }
+
+    /**
+     * Given a list of posts which are determined to be the same, return a single post which represents all of them.
+     * The new post is not saved into the database. It is assumed that the list of posts has the newest posts first.
+     */
+    private Post getPostForRepresentation(List<Post> posts) {
+        var post = new Post();
+
+        var price = posts.stream().map(Post::getPrice).filter(Optional::isPresent).map(Optional::get).findFirst();
+        price.ifPresent(post::setPrice);
+
+        var rooms = posts.stream().map(Post::getRooms).filter(Optional::isPresent).map(Optional::get).findFirst();
+        rooms.ifPresent(post::setRooms);
+
+        var constructionYear = posts.stream().map(Post::getConstructionYear).filter(Optional::isPresent).map(Optional::get).findFirst();
+        constructionYear.ifPresent(post::setConstructionYear);
+
+        var floor = posts.stream().map(Post::getFloor).filter(Optional::isPresent).map(Optional::get).findFirst();
+        floor.ifPresent(post::setFloor);
+
+        var totalFloors = posts.stream().map(Post::getTotalFloors).filter(Optional::isPresent).map(Optional::get).findFirst();
+        totalFloors.ifPresent(post::setTotalFloors);
+
+        var street = posts.stream().map(Post::getStreet).filter(Optional::isPresent).map(Optional::get).findFirst();
+        street.ifPresent(post::setStreet);
+
+        var district = posts.stream().map(Post::getDistrict).filter(Optional::isPresent).map(Optional::get).findFirst();
+        district.ifPresent(post::setDistrict);
+
+        var houseNumber = posts.stream().map(Post::getHouseNumber).filter(Optional::isPresent).map(Optional::get).findFirst();
+        houseNumber.ifPresent(post::setHouseNumber);
+
+        var heating = posts.stream().map(Post::getHeating).filter(Optional::isPresent).map(Optional::get).findFirst();
+        heating.ifPresent(post::setHeating);
+
+        var area = posts.stream().map(Post::getArea).filter(Optional::isPresent).map(Optional::get).findFirst();
+        area.ifPresent(post::setArea);
+
+        var buildingState = posts.stream().map(Post::getBuildingState).filter(Optional::isPresent).map(Optional::get).findFirst();
+        buildingState.ifPresent(post::setBuildingState);
+
+        var buildingMaterial = posts.stream().map(Post::getBuildingMaterial).filter(Optional::isPresent).map(Optional::get).findFirst();
+        buildingMaterial.ifPresent(post::setBuildingMaterial);
+
+        return post;
     }
 
     private Optional<String> getAddress(Post post) {
